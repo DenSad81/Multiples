b1435b8c291e1bf89711f410d435920af936477f
DenSad81 <tagil81@mail.ru>
2024-05-17 15:55:23 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args) {
+        int startNumber = 50;
+        int stopNumber = 150;
+        int number;
+        int quantityOfNumber = 0;
+        int minNumber = 10;
+        int maxNumber = 25;
+        Random rand = new Random();
+
+        number = rand.Next(minNumber, maxNumber + 1);
+        Console.WriteLine("Число " + number);
+        Console.WriteLine();
+
+        for (int i = 0; i <= stopNumber; i += number)
+        {
+            if (i >= startNumber && i <= stopNumber)
+            {
+                quantityOfNumber++;
+                Console.WriteLine("Кратное число " + i);
+            }
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("Количество кратных чисел " + quantityOfNumber);
+    }
+}
